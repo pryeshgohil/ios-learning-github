4a95763588253d8b050d16bf7680f04f946b2c9b
Pryesh Gohil <pryeshgohil@hotmail.com>
2022-12-14 22:31:52 +0000

    Initial Commit

---
+++ learning-github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learning-github
+//
+//  Created by Pryesh Gohil on 14/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learning-github/learning_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_githubApp.swift
+//  learning-github
+//
+//  Created by Pryesh Gohil on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct learning_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
